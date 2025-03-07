84f16658d8fa0e1e7d688a290114ea203f26548a
Pankaj Doharey <pankajdoahrey@gmail.com>
2025-03-07 23:48:40 +0530

    update timesteps

--- hybrid_unet_text_ddp.py
+++ hybrid_unet_text_ddp.py
@@ -199,7 +199,7 @@ def train(args):
 
         for batch in progress_bar:
             batch = batch.to(device, non_blocking=True)
-            timesteps = torch.randint(0, args.num_timesteps, (batch.size(0),).to(device)
+            timesteps = torch.randint(0, args.num_timesteps, (batch.size(0),), device=device)
 
             # Forward pass
             if num_gpus > 1:
